4f7a807770e2a541d3107dbdff496aecdaeb47e8
Kaan Secen <24114@ma-web.nl>
2020-12-17 10:51:49 +0100

    Quiz 1

---
+++ F1M2-JS-Quiz-Javascript-main/quiz1.js
@@ -0,0 +1,97 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/06/tilleman_20180609-0340.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat zijn de Openingstijden van het Lam Museum op zaterdag?",
+				"answers": [{
+						"answer": "11:00-17:00",
+						"feedback": true
+					},
+					{
+						"answer": "12:00-17:00",
+						"feedback": false
+					},
+					{
+						"answer": "13:00-18:00",
+						"feedback": false
+					},
+					{
+						"answer": "13:00-17:00",
+						"feedback": false
+					},
+					{
+						"answer": "13:00-19:00",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is het Instagram account van het LAM museum?",
+				"answers": [{
+						"answer": "LamLisse",
+						"feedback": false
+					},
+					{
+						"answer": "Lisse_museum",
+						"feedback": false
+					},
+					{
+						"answer": "Lisse_Lam",
+						"feedback": false
+					},
+					{
+						"answer": "lam_museum",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Waarom heeft het LAM museum geen kaartjes naast hun schilderijen?",
+				"answers": [{
+						"answer": "Omdat iedereen een eigen beeld van het schilderij heeft.",
+						"feedback": true
+					},
+					{
+						"answer": "Omdat ze het waren vergeten.",
+						"feedback": false
+					},
+					{
+						"answer": "Omdat de mensen niet veel naar schilderijen kijken.",
+						"feedback": false
+					},
+					{
+						"answer": "Omdat de kaartjes teveel geld kosten om bij alle schilderijen te plaatsen.",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoeveel euro kosten de tickets voor volwassenem?",
+				"answers": [{
+						"answer": "7.50",
+						"feedback": false
+					},
+					{
+						"answer": "5.00",
+						"feedback": false
+					},
+					{
+						"answer": "10.00",
+						"feedback": true
+					},
+					{
+						"answer": "2.50",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
