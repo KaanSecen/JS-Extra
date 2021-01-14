ae24438030e7c3cf9b789845946fa775294c3cce
Kaan Secen <24114@ma-web.nl>
2021-01-14 10:26:56 +0100

    Rotation Les6

---
+++ F1M2JS-Rotation-Sensor-main/sensor.js
@@ -0,0 +1,69 @@
+
+// activeer de Device Orientation API
+window.addEventListener('deviceorientation', handleOrientation);
+
+// event handler
+function handleOrientation(eventData){
+    let alpha, beta, gamma;
+    // alpha rotation compass rotation Z-axis
+    alpha = Math.round(eventData.alpha);
+    // beta rotation front back rotation X-axis
+    beta = Math.round(eventData.beta);
+    // gamma rotation left right rotation Y-axis
+    gamma = Math.round(eventData.gamma);
+    
+    // doe iets met de data
+    doSomething(alpha, beta, gamma);     
+}   
+
+
+function doSomething(alpha, beta, gamma){
+    showDataColor(alpha, beta, gamma); // kleurtjes en tekst
+    activateVoice(alpha, beta, gamma); // geluid
+    showMyImage(alpha, beta, gamma);   // afbeelding  
+}
+
+function showDataColor(alpha, beta, gamma){
+    //show the data in HTML
+    document.getElementById('beta').innerHTML = beta;
+    document.getElementById('gamma').innerHTML = gamma;
+    document.getElementById('alpha').innerHTML = alpha;
+
+    // change background color
+     // gamma rotation left right rotation Y-axis
+    document.body.style.background = `rgb(150, ${Math.abs(gamma)*5}, 150)`;
+}
+
+function activateVoice(alpha, beta, gamma){
+    // voice if Z-axis rotation > value
+    if(alpha > 120){
+        let myTxt = "Why you Rotate, i like Rotation";  
+        sayItLoud(myTxt); // text 2 speech
+
+        // laat het ook in de HTML zien
+        document.getElementById('voice').innerHTML = myTxt;
+    }
+    if (alpha < 20) {
+        let myTxt2 = "This is so cool wow.";  
+        sayItLoud(myTxt2); // text 2 speech
+
+        // laat het ook in de HTML zien
+        document.getElementById('voice').innerHTML = myTxt2;
+    } else {
+       // haal de tekst weg
+       document.getElementById('voice').innerHTML = ""; 
+    }
+}
+
+function showMyImage(alpha, beta, gamma){
+    // image if X-axis > value
+    if(beta > 40){
+        document.getElementById('myImage').src = "lam.jpg"; // show image
+    }
+    if ( beta < 10) {
+        document.getElementById('myImage').src = "lam2.jpg";
+    } else {
+        document.getElementById('myImage').src = ""; // no image
+    }
+
+}
--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - [x] [Array **(Website)**](http://24114.hosts1.ma-cloud.nl/JavaExtra/F1M2-JS-Javascript-3-Arrays-Txt2speech/index.html)
 - [x] [WebXR **(Website)**](http://24114.hosts1.ma-cloud.nl/JavaExtra/ma-webxr-master/index.html)
 - [x] [Quiz **(Website)**](http://24114.hosts1.ma-cloud.nl/JavaExtra/F1M2-JS-Quiz-Javascript-main/index.html)
+- [x] [Rotations **(Website Phone only)**](https://24114.hosts1.ma-cloud.nl/JavaExtra/F1M2JS-Rotation-Sensor-main/sensors.html)
 ---
 > Belangrijk: Je kan op **de tekst** klikken👍.
 ---
